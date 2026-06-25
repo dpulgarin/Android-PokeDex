76ffcf3cded91254bc0b9383db6d25fa2d2025c5
dpulgarin <dgomez.pulgarin@gmail.com>
2026-06-25 15:28:50 +0200

    Fix whitespace in UI and theme files

--- app/src/main/java/com/dpulgarin/android_pokedex/ui/theme/Theme.kt
+++ app/src/main/java/com/dpulgarin/android_pokedex/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.dpulgarin.android_pokedex.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
